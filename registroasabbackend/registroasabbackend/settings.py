d0969c0170e8c688c8fec14dfa4d7b30cba8a8ab
fabiandres9106 <fabiandres9106@gmail.com>
2024-05-27 02:28:55 -0500

    Se pasó a producción

--- registroasabbackend/registroasabbackend/settings.py
+++ registroasabbackend/registroasabbackend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&-=)+)$#k5uby9!)e*8d*2z8i+8iq!c=!_%a%%)(o=@w^w*cf+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

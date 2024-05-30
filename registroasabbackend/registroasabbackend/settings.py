07d2b2f7e71ce50812b61b3e989be817a348173c
fabiandres9106 <fabiandres9106@gmail.com>
2024-05-29 23:39:56 -0500

    Configuración mysql

--- registroasabbackend/registroasabbackend/settings.py
+++ registroasabbackend/registroasabbackend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&-=)+)$#k5uby9!)e*8d*2z8i+8iq!c=!_%a%%)(o=@w^w*cf+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'registroasabbackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'registro_asab',
+        'USER': 'registro_asab',
+        'PASSWORD': '*5YJGZjUybR3RDdi',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 

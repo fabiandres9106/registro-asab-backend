3e94037db34a9a232fb1eec5a488557fb23785c5
fabiandres9106 <fabiandres9106@gmail.com>
2024-05-29 21:48:18 -0500

    Se agregó https://registro.cajanegrateatro.com.co dentro de las url permitidas en CORS

--- registroasabbackend/registroasabbackend/settings.py
+++ registroasabbackend/registroasabbackend/settings.py
@@ -130,5 +130,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",  # URL del frontend de React
     "https://api.cajanegrateatro.com.co",  # URL del dominio de producción
-
+    "https://registro.cajanegrateatro.com.co"
 ]
